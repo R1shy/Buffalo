d02f563c1d4471265a9d2fd7156e0c6208fa464a
Rishy <kadlecekemma@gmail.com>
2025-10-03 12:19:14 -0700

    .

---
+++ kernel/src/lib.rs
@@ -0,0 +1,3 @@
+#![feature(abi_x86_interrupt)] 
+#![no_std]
+pub mod interrupts;
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,6 +9,7 @@ use conquer_once::spin::OnceCell;
 use core::panic::PanicInfo;
 
 
+
 pub static BOOTLOADER_CONFIG: BootloaderConfig = {
     let mut config = BootloaderConfig::new_default();
     config.kernel_stack_size = 90 * 1024;
@@ -29,23 +30,14 @@ entry_point!(main, config = &BOOTLOADER_CONFIG);
 
 
 fn main(bootinfo: &'static mut bootloader_api::BootInfo) -> ! {
-    // free the doubly wrapped framebuffer from the boot info struct
     let frame_buffer_optional = &mut bootinfo.framebuffer;
-    
-    // free the wrapped framebuffer from the FFI-safe abstraction provided by bootloader_api
     let frame_buffer_option = frame_buffer_optional.as_mut();
-    
-    // unwrap the framebuffer
     let frame_buffer_struct = frame_buffer_option.unwrap();
-    
-    // extract the framebuffer info and, to satisfy the borrow checker, clone it
     let frame_buffer_info = frame_buffer_struct.info().clone();
-    
-    // get the framebuffer's mutable raw byte slice
     let raw_frame_buffer = frame_buffer_struct.buffer_mut();
-    
-    // finally, initialize the logger using the last two variables
     init_logger(raw_frame_buffer, frame_buffer_info);
+
+    
 
     loop {}
 }
